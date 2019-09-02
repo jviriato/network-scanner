19190caa89df1addcd000fd6ec433ad1cd3f7607
jviriato <josevviriato@gmail.com>
2019-09-01 23:05:19 -0300

    Descobrir o fabricante através do MAC

---
+++ mac_finder.py
@@ -0,0 +1,20 @@
+class Macfinder:
+    def __init__(self, MAC):
+        self.MAC = MAC
+        self.manufacturer = self.parseManuf()
+
+    def parseManuf(self):
+        """ Trata o arquivo Manuf e retorna um dicionário com
+        o MAC e seu fabricante
+
+        Returns:
+            Object -- Dicionário contendo MAC e nome do fabricante
+        """
+        d = {}
+        with open("manuf") as f:
+            for line in f:
+                line_parsed = line.split("\t", 1)
+                if (line[0] is not '#' and len(line_parsed) > 1):
+                    (key, val) = line_parsed
+                    d[key] = val
+        return d
