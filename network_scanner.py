fce5dcd268c0d21c7ebf7734ef687acb90a67472
jviriato <josevviriato@gmail.com>
2019-09-02 00:09:03 -0300

    Adiciona método para escrever o csv

--- network_scanner.py
+++ network_scanner.py
@@ -1,11 +1,30 @@
 
 import subprocess
+import socket
 import csv
+import os
+from datetime import date
 from mac_finder import MacFinder
 from device import Device
-from datetime import date
 class NetworkScanner:
     """ Classe para realizar o scan da rede.
     """
     def __init__(self):
         self.today = date.today()
+
+    def getOwnIpAddress(self):
+        """Retorna o IP do usuário
+        Returns:
+            str -- IP do usuário formatado
+        """
+        hostname = socket.gethostname()
+        ipAddr = socket.gethostbyname(hostname)
+        return ipAddr
+    
+    def createCSV(self):
+        """ Cria o csv que mantém o histórico de dispositivos numa rede
+        """
+        firstRow = ['ID', 'IP', 'MAC', 'Fabricante', 'Hora Descoberta']
+        with open('historico.csv', 'a') as h:
+            writer = csv.writer(h)
+            writer.writerow(firstRow)
